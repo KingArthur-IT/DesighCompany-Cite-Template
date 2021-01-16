d92eb03fcd75a78208e9264cce6526a15c3178b8
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-01-16 20:59:49 +0200

    Написаны скрипты для слайдера

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.slider-panel__head__btns__left').on('click', function(){
+    let w =  $('.slider-panel__main').css('width');
+    let width = Number(w.slice(0, w.length - 2)) / 3;
+    let m =  $('.slider-panel__main').css('margin-left');
+    let margin = Number(m.slice(0, m.length - 2));
+    if (margin > - 2.5 * width && margin < - 0.5 * width)
+    {
+        let left = margin + width;
+        $('.slider-panel__main').css('margin-left',left + 'px');
+    }//if
+});
+
+$('.slider-panel__head__btns__right').on('click', function(){
+    let w =  $('.slider-panel__main').css('width');
+    let width = Number(w.slice(0, w.length - 2)) / 3;
+    let m =  $('.slider-panel__main').css('margin-left');
+    let margin = Number(m.slice(0, m.length - 2));
+    if (margin < 0.5 * width && margin > -1.5 * width)
+    {
+        let left = margin - width;
+        $('.slider-panel__main').css('margin-left', left + 'px');
+    }//if
+});
+
+$(window).resize(function(){
+    $('.slider-panel__main').css('margin-left', '0px');
+});
