0465a5f048c1a029a7759d1f7d78234ed8566eb7
KingArthur-IT <67517973+KingArthur-IT@users.noreply.github.com>
2021-01-17 15:10:49 +0200

    Создано модальное окно авторизации

--- js/main.js
+++ js/main.js
@@ -27,3 +27,17 @@ $('.slider-panel__head__btns__right').on('click', function(){
 $(window).resize(function(){
     $('.slider-panel__main').css('margin-left', '0px');
 });
+
+//Создание модельного окна
+$(document).ready(function(){
+    $(".modalbox").fancybox(); //Показать модальное окно
+    $(".signup-box__form").submit(function(){ return false; });
+    $(".signup-box__form__btn").on("click", function(){         
+        // действия по обработке формы
+        // закрываем окно, как правило делать это нужно после обработки данных
+        $(".signup-box__form").fadeOut("fast", function(){
+            $(this).before("<p><strong>В демо режиме авторизация не работает, но спасибо, что дошли до этого момента!</strong></p>");
+            setTimeout("$.fancybox.close()", 5000);
+        });
+    });
+});
